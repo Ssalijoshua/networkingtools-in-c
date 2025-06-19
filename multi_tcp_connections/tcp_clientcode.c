89b4862304897564c8b84335665480ff469526b2
Ssalijoshua <ssalijoshua2002@gmail.com>
2025-06-19 11:17:03 +0300

    Resuming

--- multi_tcp_connections/tcp_clientcode.c
+++ multi_tcp_connections/tcp_clientcode.c
@@ -6,7 +6,7 @@
 
 
 int main(int argc, char *argv[]){
-    
+    // Initialize Winsock just incase it is what the user is using.
 #if defined(_WIN32)
     WSADATA d;
     if (WSAStartup(MAKEWORD(2, 2), &d)) {
@@ -14,5 +14,12 @@ int main(int argc, char *argv[]){
         return 1;
     }
 #endif
+
+// checking to see if we have the required number of arguments
+if (argc < 3){
+    fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+    return 1;
+}
+
 
 }
